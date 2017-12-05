763d85416512443b4c0d6498ec4da5799adc837d
Andy Gelnar <apgelnar@gmail.com>
2017-12-05 12:57:31 +0000

    comlete 2017 day 5

---
+++ 2017/5/solve_2.py
@@ -0,0 +1,19 @@
+from itertools import count
+
+with open('input.txt') as fandle:
+    instructions = [int(i) for i in fandle]
+
+index = 0
+try:
+    # count starts at 0, when we loop again we count the last instuction.
+    # the IndexError will be raised when trying to execude the first
+    # instruction outside of the list.
+    for steps in count():
+        jump = instructions[index]
+        instructions[index] += -1 if jump >= 3 else 1
+        index += jump
+
+except IndexError:
+    pass
+
+print steps
