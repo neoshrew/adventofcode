f16b46054588d38598c5dc39c69fdebcab903a2e
Andy Gelnar <neoshrew@users.noreply.github.com>
2022-12-04 11:02:25 +0000

    2022 day 4

---
+++ 2022/4/solve_2.py
@@ -0,0 +1,30 @@
+import re
+
+INPUT_FNAME = "input.txt"
+# INPUT_FNAME = "test_1.txt"
+
+_LINE_RE = re.compile("^(\d+)-(\d+),(\d+)-(\d+)$")
+def get_pairings():
+    with open(INPUT_FNAME) as fandle:
+        for line in fandle:
+            # 93-94,34-93
+            ns = list(map(int, _LINE_RE.match(line).groups()))
+            yield ((ns[0], ns[1]), (ns[2], ns[3]))
+
+def main():
+    total = 0
+
+    # I could do a faster start/end range comparison.
+    # I could not use loads of memory for no reason.
+    # but I'm lazy.
+    for elf1, elfA in get_pairings():
+        elf1 = set(range(elf1[0], elf1[1]+1))
+        elfA = set(range(elfA[0], elfA[1]+1))
+        if elf1 & elfA:
+            total += 1
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
