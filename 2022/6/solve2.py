d91d0d49764187d39cb07f26b6d28b0b209e0cdd
Andy Gelnar <neoshrew@users.noreply.github.com>
2022-12-06 22:08:54 +0000

    2022 day 6
    ez, what even is going on this year?

---
+++ 2022/6/solve2.py
@@ -0,0 +1,16 @@
+INPUT_FNAME = "input.txt"
+# INPUT_FNAME = "test_1.txt"
+
+PATTERN_LEN = 14
+
+def main():
+    with open(INPUT_FNAME) as fandle:
+        data = fandle.read()
+
+    for i in range(PATTERN_LEN, len(data)):
+        if len(set(data[i-PATTERN_LEN:i])) == PATTERN_LEN:
+            print(i)
+            break
+
+if __name__ == "__main__":
+    main()
